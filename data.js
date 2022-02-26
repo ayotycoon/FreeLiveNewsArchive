6ffd44c9e27adc15ea47167ea495508604a04e7a
ayotycoon <kaysehventeen@gmail.com>
2022-02-26 17:12:55 -0600

    c

--- data.js
+++ data.js
@@ -1,22 +1,9 @@
 const el = document.querySelector("#placement")
 const data = [
-    {
-        country: "General",
 
-        sites: [
-            {
-                name: "Aljazeera",
-                link: "https://www.aljazeera.com/live/"
-            },
-            {
-                name: "NBC News",
-                link: "https://www.youtube.com/c/NBCNews"
-            }
-     
-        ]
-    },
     {
         country: "UK",
+        img:"gb.svg",
 
         sites: [
             {
@@ -34,7 +21,7 @@ const data = [
     },
     {
         country: "US",
-
+        img:"us.svg",
         sites: [
             {
                 name: "CNN 1",
@@ -54,15 +41,47 @@ const data = [
             },
      
         ]
-    }
+    },
+    {
+        country: "Nigeria",
+        img:"ng.svg",
+        sites: [
+            {
+                name: "Arise Tv",
+                link: "https://www.arise.tv/live-tv/"
+            },
+     
+     
+        ]
+    },
+    {
+        country: "Middle East",
+    
+
+        sites: [
+            {
+                name: "Aljazeera",
+                link: "https://www.aljazeera.com/live/"
+            },
+
+            {
+                name: "NBC News",
+                link: "https://www.youtube.com/c/NBCNews"
+            }
+     
+        ]
+    },
 ]
 
 let html = "";
 data.forEach(c => {
     let cell = "";
     
-    c.sites.forEach(s => {
+    c.sites.forEach((s,i) => {
+        const group = i %2 == 0;
+        const end = i %2 != 0 || i == c.sites.length-1;
         cell+= `
+        ${group ? `<div class="inner">` : '' }
         <div>
 <div id="image18" class="style2 image"><span class="frame"><img
             src="assets/images/image18.svg?v=2b99aba4" alt=""></span></div>
@@ -72,6 +91,7 @@ data.forEach(c => {
     <li class="n01"><a target="_blank" href="${s.link}">Go To</a></li>
 </ul>
 </div>
+${end ? `</div>` : '' }
 `
       
     });
@@ -85,7 +105,11 @@ data.forEach(c => {
         <div class="inner">
             <div class="before-spacer">
                 <hr id="divider03" class="style1">
-                <h2 id="text85" class="style3">${c.country}</h2>
+                <h2 id="text85" class="style3">
+                
+                ${c.country}
+                ${c.img ? `<img src="${c.img}" style="width:30px" />`:''}
+                </h2>
 
             </div><span></span>
         </div>
@@ -93,11 +117,11 @@ data.forEach(c => {
 </div>
 <div id="container14" class="container columns full screen">
     <div class="wrapper">
-        <div class="inner">
+     
 ${cell}
 
    
-        </div>
+      
     </div>
 </div>
 
